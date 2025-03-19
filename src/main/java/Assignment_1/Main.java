bead7c989f37b82a5af12525e2d104afe394e76f
Chandra Sekhar Vipparla <vipparlachandrasekhar@gmail.com>
2025-03-19 14:03:31 +0530

    final commit for Assignment 1

---
+++ src/main/java/Assignment_1/Main.java
@@ -0,0 +1,22 @@
+package Assignment_1;
+
+//MAIN  METHOD WITH CONSTRUCTOR INJECTION
+//public class Main {
+//    public static void main(String[] args) {
+//        //DEPENDENCY CREATION
+//        NotificationService service = new PushNotificationService();
+//        //DEPENDENCY INJECTION
+//        NotificationProcessor processor = new NotificationProcessor(service);
+//        processor.process("A new Biryani is waiting for you.!!");
+//    }
+//}
+
+//MAIN METHOD WITH SETTER INJECTION
+public class Main{
+    public static void main(String[] args) {
+        NotificationService service = new VoiceNotificationService();
+        NotificationProcessor processor = new NotificationProcessor();
+        processor.setNotificationService(service);
+        processor.process("A new Chicken Biryani is waiting for you");
+    }
+}
--- src/main/java/Assignment_1/NotificationProcessor.java
+++ src/main/java/Assignment_1/NotificationProcessor.java
@@ -1,8 +1,19 @@
 package Assignment_1;
 
+//NOTIFICATION PROCESSOR FOR CONSTRUCTOR INJECTION
+//public class NotificationProcessor {
+//    private NotificationService notificationService;
+//    public NotificationProcessor(NotificationService notificationService){
+//        this.notificationService = notificationService;
+//    }
+//    public void process(String message){
+//        notificationService.notify(message);
+//    }
+//}
+// NOTIFICATION PROCESSOR FOR SETTER INJECTION
 public class NotificationProcessor {
     private NotificationService notificationService;
-    public NotificationProcessor(NotificationService notificationService){
+    public void setNotificationService(NotificationService notificationService){
         this.notificationService = notificationService;
     }
     public void process(String message){
